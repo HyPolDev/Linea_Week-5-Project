9c5557c093dc9ef2dcf60aa03e0936e99fd9c133
Pol <polmonteroalegre@gmail.com>
2024-03-08 22:02:21 +0100

    FOUNDATIONS: server.js and db connection created and working successfuly

---
+++ src/server.js
@@ -0,0 +1,36 @@
+import express from "express";
+import 'dotenv/config'
+import { dbConnection } from "./database/db.js";
+//import router from './routes/router.js'
+
+const app = express()
+
+app.use(express.json())
+
+const PORT = process.env.PORT || 4001;
+
+app.get('/healthy', (req, res) => {
+    res.status(200).json(
+        {
+            success: true,
+            message: "server is healthy"
+        }
+    )
+})
+
+//app.use('/api', router)
+
+dbConnection()
+    .then(() => {
+        console.log("Database connected");
+
+        app.listen(PORT, () => {
+            console.log(`Server running o port ${PORT}`);
+        })
+    })
+    .catch(error => {
+        console.log(error);
+    })
+
+
+
