b2862eeaa5db2dc147619d57895be79e48bab5a2
Pol <polmonteroalegre@gmail.com>
2024-03-12 12:24:01 +0100

    FEAT: deleteprofile for is admin or self created and tested; isselfor admin created

--- src/entities/users/user.controller.js
+++ src/entities/users/user.controller.js
@@ -1,5 +1,5 @@
 import { handleError } from "../../core/handleError.js"
-import { getProfileService, getUsersService } from "./user.service.js"
+import { deleteProfileService, getProfileService, getUsersService } from "./user.service.js"
 
 export const getUsers = async (req, res) => {
     try {
@@ -33,11 +33,28 @@ export const getProfile = async (req, res) => {
         })
 
     } catch (error) {
-        if (error.message === "Users not found" ||
-            `No users found at page ${req.body.page}`
-        ) {
+        if (error.message === "Users not found") {
             return handleError(res, error.message, 400)
         }
         handleError(res, "Cant get users, server error", 500)
+    }
+}
+
+export const deleteProfile = async (req, res) => {
+    try {
+        const profile = await deleteProfileService(req)
+
+        res.status(200).json({
+            success: true,
+            message: "Profile deleted successfuly",
+            profile: profile
+        })
+
+    } catch (error) {
+        if (error.message === "Users not found" ||
+            "User already deleted") {
+            return handleError(res, error.message, 400)
+        }
+        handleError(res, "Can not delete profile", 500)
     }
 }
---
+++ src/validator/isSelfOrAdmin.js
@@ -0,0 +1,18 @@
+export const isSelfOrAdmin = (req, res, next) => {
+    try {
+        if (req.tokenData.roleName !== "admin"
+            && req.tokenData.userId !== parseInt(req.params.id)) {
+            return res.status(401).json({
+                success: false,
+                message: "Unauthorized"
+            })
+        }
+        next()
+
+    } catch (error) {
+        res.status(500).json({
+            success: false,
+            message: "Role not valid"
+        })
+    }
+}
