a00c5d16197e65f7e19143595a830e5bedb54928
Pol <polmonteroalegre@gmail.com>
2024-04-09 11:00:05 +0200

    FEAT: update cors

--- src/core/server.js
+++ src/core/server.js
@@ -1,11 +1,14 @@
-import express from "express";
-import 'dotenv/config'
 import { dbConnection } from "../database/db.js";
+import express from "express"
 import router from './router.js'
+import cors from 'cors'
+import 'dotenv/config'
+
 
 const app = express()
 
 app.use(express.json())
+app.use(cors())
 
 const PORT = process.env.PORT || 4001;
 
